9f3d851e7d3e5b24dfd5d7bbd9a5b7f40b5c1d96
Anthony Hamon <anthony.hamon91@gmail.com>
2025-01-19 16:46:07 +0100

    refactor: changed project structure due to full design switch

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,26 +1,20 @@
-import { Component, inject, Input } from '@angular/core';
-import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
-import { ApiService } from '../services/api.service';
-import { PokemonClient } from 'pokenode-ts';
-import { HeaderComponent } from './header/header.component';
+import { Component} from '@angular/core';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { PokemonOverviewComponent } from './pokemon-overview/pokemon-overview.component';
 
 
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [PokemonCardComponent, HeaderComponent],
+  imports: [TopBarComponent, PokemonListComponent, PokemonOverviewComponent],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.scss'
 })
 export class PokedexComponent {
 
-  PokeService = inject(ApiService);
-  query = this.PokeService.query;
-
-  constructor(){
-    this.PokeService.getQueryPokemons(this.query);
-  }
+ 
 
 
 
