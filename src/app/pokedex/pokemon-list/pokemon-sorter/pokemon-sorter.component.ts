978dae25543eda7ca71f6d4305d6d758a2053b85
Anthony Hamon <anthony.hamon91@gmail.com>
2025-01-19 16:54:04 +0100

    feat: new component to sort pokemons in the list

---
+++ src/app/pokedex/pokemon-list/pokemon-sorter/pokemon-sorter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-sorter',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-sorter.component.html',
+  styleUrl: './pokemon-sorter.component.scss'
+})
+export class PokemonSorterComponent {
+
+}
