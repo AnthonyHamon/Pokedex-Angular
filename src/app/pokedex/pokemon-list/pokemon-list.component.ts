688219c33bd66ede3281508e575c03c217b5c0ce
Anthony Hamon <anthony.hamon91@gmail.com>
2025-01-19 16:51:14 +0100

    style: started with style of pokemon-list

--- src/app/pokedex/pokemon-list/pokemon-list.component.ts
+++ src/app/pokedex/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { ApiService } from '../../services/api.service';
+import { PokemonSorterComponent } from './pokemon-sorter/pokemon-sorter.component';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [PokemonSorterComponent, PokemonCardComponent],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss'
 })
 export class PokemonListComponent {
 
+
+  PokeService = inject(ApiService);
+  query = this.PokeService.query;
+
+  constructor() {
+    this.PokeService.getQueryPokemons(this.query);
+  }
+
+  
 }
