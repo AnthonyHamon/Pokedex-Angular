5940b4e24d642db3ea08503a611edc288ad3566f
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-24 17:28:33 +0100

    feat: update service to render pokemon and its information by click on it

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -1,16 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Pokemon, PokemonSpecies } from 'pokenode-ts';
 import { BehaviorSubject } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  private dataSource = new BehaviorSubject<string>('');
-  currentPokemon = this.dataSource.asObservable();
+  pokeService = inject(ApiService);
 
-  renderPokemonInformations(data: string) {
-    this.dataSource.next(data);
+
+  private pokemonDataSource = new BehaviorSubject<Pokemon | undefined>(undefined);
+  currentPokemon = this.pokemonDataSource.asObservable();
+
+  private speciesDataSource = new BehaviorSubject<PokemonSpecies | undefined>(undefined);
+  currentSpecies = this.speciesDataSource.asObservable();
+
+  renderPokemonInformations(data: Pokemon) {
+    this.getPokemonMainInformations(data);
+    this.getPokemonSpeciesInformations(data);
+  }
+
+  getPokemonMainInformations(data: Pokemon) {
+    this.pokemonDataSource.next(data);
+  }
+
+  async getPokemonSpeciesInformations(data: Pokemon) {
+    const species = await this.pokeService.getPokemonSpecies(data);
+    this.speciesDataSource.next(species);
   }
 
 }
