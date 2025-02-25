24c48c7bf8ad0cbea6d5c3695eba4d6ad7bca89b
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-25 21:17:44 +0100

    feat: added function to receive pokemon information from api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,44 +11,50 @@ export class ApiService {
   pokedex = new GameClient();
 
   pokemonCount: number = 0;
-  pokemonsList:any = [];
-  loadedPokemons:any = [];
-  query:number = 0;
+  pokemonsList: any = [];
+  loadedPokemons: any = [];
+  query: number = 0;
 
   constructor() {
-    this.getAllPokemonsDatas();    
+    this.getAllPokemonsDatas();
   }
 
   async getAllPokemonsDatas() {
-    try{
+    try {
       const pokedex = await this.pokedex.getPokedexById(POKEDEXES.NATIONAL);
       this.pokemonCount = (pokedex).pokemon_entries.length;
       const data = await this.api.listPokemons(0, this.pokemonCount);
-      for (const pokemon of data.results){
-        const pokemonData = await this.returnPokemonData(pokemon.name);
+      for (const pokemon of data.results) {
+        const pokemonData = await this.getPokemonData(pokemon.name);
         this.pokemonsList.push(pokemonData);
-        console.log(pokemonData);
       }
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   }
 
-  async getQueryPokemons(query: number = 0, limit:number = 50) {
-    try{
+  async getQueryPokemons(query: number = 0, limit: number = 100) {
+    try {
       const data = await this.api.listPokemons(query, limit);
-      for (const pokemon of data.results){
-        const pokemonData = await this.returnPokemonData(pokemon.name);
+      for (const pokemon of data.results) {
+        const pokemonData = await this.getPokemonData(pokemon.name);
         this.loadedPokemons.push(pokemonData);
       }
-      console.log(this.loadedPokemons);
-    }catch(error) {
+    } catch (error) {
       console.log(error);
     }
   }
 
-  async returnPokemonData(pokemonName:any){
-      const pokemonData = await this.api.getPokemonByName(pokemonName);
-      return pokemonData;
-      }
+  async getPokemonData(pokemonName: any) {
+    const pokemonData = await this.api.getPokemonByName(pokemonName);
+    return pokemonData;
+  }
+
+
+  async getPokemonSpecies(pokemonData: any) {
+    const pokemonSpecies = await this.api.getPokemonSpeciesByName(pokemonData?.name);
+    console.log(pokemonSpecies);
+    return pokemonSpecies;
+  }
+
 }
